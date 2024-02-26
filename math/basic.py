89a8c2c99a9d0c65463fea4ef50810f4b67b4fba
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-02-26 12:42:22 -0800

    Create basic.py

---
+++ math/basic.py
@@ -0,0 +1,21 @@
+"""
+Module file with functions for squaring numbers, multiplying numbers by 2, and summing 2 inputs
+"""
+
+def square(num):
+    """
+    Takes a number as input, and returns the square of that number
+    """
+    return num ** 2
+
+def double(num):
+    """
+    Takes a number as input, and returns that number multiplied by 2
+    """
+    return num * 2
+
+def add(x, y):
+    """
+    Takes 2 inputs, and returns their sum
+    """
+    return x + y
