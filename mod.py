b10d90970f82009ac909373c2eceafd482cb62c6
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-02-26 11:57:33 -0800

    Create mod.py

---
+++ mod.py
@@ -0,0 +1,15 @@
+"""
+Module file with functions for squaring numbers and multiplying numbers by 2
+"""
+
+def square(num):
+    """
+    Takes a number as input, and returns the square of that number
+    """
+    return num ** 2
+
+def double(num):
+    """
+    Takes a number as input, and returns that number multiplied by 2
+    """
+    return num * 2
