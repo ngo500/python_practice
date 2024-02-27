c73af77aa4e28a141d2b15119dd85bf152927f01
ngo500 <72844093+ngo500@users.noreply.github.com>
2024-02-27 10:35:40 -0800

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,7 +1,10 @@
 from flask import Flask     #import flask class from module
+#from flask import jsonify  #import JSONify from flask to use JSONify
 app = Flask(__name__)       #instantiate an object as your class
 
 #pass in URL path, return HTML in method
 @app.route('/')
 def hello_world():
-    return "<b> Test Flask application </b>"
+    return "<b> Test Flask application </b>"            #return HTML
+    #return {"message" : "Hello World Dictionary!"}     #return JSON using dictionary
+    #return jsonify(message = "Hello World JSONify!")   #return JSON using JSONify
